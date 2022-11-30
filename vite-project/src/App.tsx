d4e262d467fbe073663660e03537eee952a87023
mcdmid <cameronmcdermid@live.com>
2022-11-30 16:49:06 +0000

    Random colour generated on refresh.

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return <div className="App">
+    <div className='guess-me'></div>
+  </div>;
+}
+
+export default App;
+
