5022c90abf4a8030197df41b154746a34e3cb57d
mcdmid <cameronmcdermid@live.com>
2022-12-02 11:03:47 +0000

    Colour randomised

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
+  const [color, setColor] = useState(""); 
+  const [answers, setAnswers] = useState<string[]>([]);
 
-  return <div className="App">
-    <div className='guess-me'></div>
-  </div>;
-}
+
+  const getRandomColor = () => {
+    const digits =[ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", ];
+
+    const color = new Array(6)
+      .fill("")
+      .map(() => digits[Math.floor(Math.random() * digits.length)])
+      .join("");
+
+      return `#${color}`;
+  };
+
+  useEffect(() => {
+    const actualColor = getRandomColor();
+    setColor(actualColor);
+    setAnswers([actualColor, getRandomColor(), getRandomColor()].sort(() => 0.5 - Math.random()));
+  }, []);
+
+  return ( 
+  <div className="App">
+    <div className='guess-me' style={{ background: color }}></div>
+  </div>
+  );
+} 
 
 export default App;
 
